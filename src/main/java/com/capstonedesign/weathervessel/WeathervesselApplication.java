12646b2d88644c451efe5ecacc1539e1a80d0318
KimMinSu-PC\KimMinSu <his4000@hanmail.net>
2017-09-01 15:23:55 +0900

    Init web service for spring boot

---
+++ src/main/java/com/capstonedesign/weathervessel/WeathervesselApplication.java
@@ -0,0 +1,12 @@
+package com.capstonedesign.weathervessel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathervesselApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathervesselApplication.class, args);
+	}
+}
