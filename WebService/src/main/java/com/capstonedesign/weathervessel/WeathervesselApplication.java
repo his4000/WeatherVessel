9c78017c6d3d519e6aa6a447952e5c26c20e5921
KimMinSu-PC\KimMinSu <his4000@hanmail.net>
2017-09-02 11:41:15 +0900

    Spring test

--- WebService/src/main/java/com/capstonedesign/weathervessel/WeathervesselApplication.java
+++ WebService/src/main/java/com/capstonedesign/weathervessel/WeathervesselApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeathervesselApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(WeathervesselApplication.class, args);
+		System.out.println("Hello Spring");
 	}
 }
