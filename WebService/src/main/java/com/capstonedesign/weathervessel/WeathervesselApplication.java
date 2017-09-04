154d7592f165729092828610ff426b44b9ef5074
KimMinSu-PC\KimMinSu <his4000@hanmail.net>
2017-09-05 08:18:08 +0900

    Modify class path error

--- WebService/src/main/java/com/capstonedesign/weathervessel/WeathervesselApplication.java
+++ WebService/src/main/java/com/capstonedesign/weathervessel/WeathervesselApplication.java
@@ -1,9 +1,11 @@
 package com.capstonedesign.weathervessel;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class WeathervesselApplication {
 
 	public static void main(String[] args) {
