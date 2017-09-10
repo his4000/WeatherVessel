6a6e8772dd8cada94963837abc9cfa909cb68c82
KimMinSu-PC\KimMinSu <his4000@hanmail.net>
2017-09-11 00:50:27 +0900

    Add log

--- WebService/src/main/java/com/capstonedesign/weathervessel/web/WeathervesselController.java
+++ WebService/src/main/java/com/capstonedesign/weathervessel/web/WeathervesselController.java
@@ -1,12 +1,14 @@
 package com.capstonedesign.weathervessel.web;
 
 import com.capstonedesign.weathervessel.service.RequestMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
+@Slf4j
 public class WeathervesselController {
 
     private String hello = "Project Weather Vessel for Capstone Design 2 class in Konkuk Univ. Department of Computer Science Engineering - KIM MIN SU, LEE CHANG OH, CHO YOON KI";
@@ -18,11 +20,14 @@ public class WeathervesselController {
 
     @RequestMapping("/keyboard")
     public String keyboard(){
+        log.info("keyboard controller running");
         return "{\"type\" : \"buttons\", \"buttons\" : [\"선택 1\", \"선택 2\", \"선택 3\"]}";
     }
 
     @RequestMapping("/message")
     public RequestMessage reply(String user_key) {
+        log.info("message controller running");
+
         RequestMessage requestMessage = new RequestMessage();
 
         requestMessage.setUser_key(user_key);
