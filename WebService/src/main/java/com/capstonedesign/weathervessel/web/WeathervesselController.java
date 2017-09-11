65819c97524bbf749c5e154e7ad4d110ea077bbb
KimMinSu-PC\KimMinSu <his4000@hanmail.net>
2017-09-11 15:15:15 +0900

    Add logging

--- WebService/src/main/java/com/capstonedesign/weathervessel/web/WeathervesselController.java
+++ WebService/src/main/java/com/capstonedesign/weathervessel/web/WeathervesselController.java
@@ -34,6 +34,8 @@ public class WeathervesselController {
     public ResponseMessage message(@RequestBody RequestMessage requestMessage){
         ResponseMessage responseMessage = new ResponseMessage();
 
+        log.info(requestMessage.toString());
+
         if(requestMessage.getContent().isEmpty())
             responseMessage.setMessage(new Message("뭐라구요? 잘 안들려요"));
         else
@@ -41,24 +43,4 @@ public class WeathervesselController {
 
         return responseMessage;
     }
-
-    /*@RequestMapping(value = "/message", method = RequestMethod.POST, headers = "Accept=application/json")
-    @ResponseBody
-    public String reply(@RequestBody JSONObject resObj) throws Exception {
-        log.info("message controller running");
-        log.info("resObj : " + resObj.toString());
-
-        String content;
-        content = (String) resObj.get("content");
-        JSONObject jsonRes = new JSONObject();
-        JSONObject jsonText = new JSONObject();
-
-        if(content != null)
-            jsonText.put("text", "잘 들려요");
-        else
-            jsonText.put("text", "뭐라구요? 잘 안들려요");
-        jsonRes.put("message", jsonText);
-
-        return jsonRes.toString();
-    }*/
 }
