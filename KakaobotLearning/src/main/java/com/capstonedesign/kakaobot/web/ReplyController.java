46e52633ef8e6691189d67dc744eeb3e51f34315
KimMinSu-PC\KimMinSu <his4000@hanmail.net>
2017-09-15 11:41:58 +0900

    Change from RestController to Controller and ResponseBody

--- KakaobotLearning/src/main/java/com/capstonedesign/kakaobot/web/ReplyController.java
+++ KakaobotLearning/src/main/java/com/capstonedesign/kakaobot/web/ReplyController.java
@@ -5,12 +5,13 @@ import com.capstonedesign.kakaobot.service.Brain;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 @Slf4j
 public class ReplyController {
@@ -19,11 +20,13 @@ public class ReplyController {
     QuestionsRepository questionsRepository;
 
     @RequestMapping(value = "/getText", method = RequestMethod.GET)
+    @ResponseBody
     public String index(){
         return "index";
     }
 
     @RequestMapping(value = "/getText", method = RequestMethod.POST)
+    @ResponseBody
     public String getText(@RequestBody String text) throws Exception {
         Brain brain = new Brain();
         log.info("get text : " + text);
