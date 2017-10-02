6c13c0976b8c5ffbccd4383db9e425abdb6ed904
KimMinSu-PC\KimMinSu <his4000@hanmail.net>
2017-10-02 20:28:55 +0900

    Change nlp

--- KakaobotLearning/src/main/java/com/capstonedesign/kakaobot/service/natural_language_processing/NaturalLanguageProcessing.java
+++ KakaobotLearning/src/main/java/com/capstonedesign/kakaobot/service/natural_language_processing/NaturalLanguageProcessing.java
@@ -4,10 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.openkoreantext.processor.KoreanTokenJava;
 import org.openkoreantext.processor.OpenKoreanTextProcessorJava;
 import org.openkoreantext.processor.phrase_extractor.KoreanPhraseExtractor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -15,39 +13,6 @@ import static java.util.stream.Collectors.toList;
 @Service
 @Slf4j
 public class NaturalLanguageProcessing{
-
-    //@Autowired
-    //AddressRepository addressIndexRepository;
-
-    //@PostConstruct
-    /*public void initNLP(){
-        List<String> filePath = new ArrayList<>();
-        filePath.add("./src/main/resources/static/address/Seoul.txt");
-        List<String> addressList = new ArrayList<>();
-
-        log.info(System.getProperty("user.dir"));
-
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath.get(0)), "UTF8"))){
-            String line;
-
-            while((line = br.readLine()) != null){
-                String[] tokens = line.split(",|:");
-                addressList.addAll(new ArrayList<>(Arrays.asList(tokens)));
-            }
-            br.close();
-        }catch (Exception e){
-            log.info("Init address Error");
-            e.printStackTrace();
-        }
-*/
-        /*KoreanDictionaryProvider.koreanDictionary().put(KoreanPos.Space(), KoreanConjugation.conjugatePredicatesToCharArraySet(KoreanDictionaryProvider.readWordsAsSet(
-                JavaConverters.asScalaBuffer(filePath)
-        ), false));*/
-
-     /*   KoreanDictionaryProvider.koreanDictionary().put(KoreanPos.Space(), KoreanDictionaryProvider.readWords());
-
-        OpenKoreanTextProcessorJava.addWordsToDictionary(KoreanPosJava.Space, addressList.stream().filter(address -> !address.equalsIgnoreCase("")).collect(toList()));
-    }*/
 
     public List<KoreanTokenJava> textTokenizing(String text){
         return OpenKoreanTextProcessorJava.tokensToJavaKoreanTokenList(
@@ -68,20 +33,4 @@ public class NaturalLanguageProcessing{
         return phrases.stream().map(KoreanPhraseExtractor.KoreanPhrase::text).collect(toList());
     }
 
-    /*public String getAddressFromText(String text){
-        List<KoreanTokenJava> tokens = textTokenizing(text);
-        List<String> addressList = new ArrayList<>();
-        String ret = "";
-
-        for(KoreanTokenJava token : tokens){
-            if(addressIndexRepository.findByAddress(token.getText()) != null)
-                addressList.add(token.getText());
-        }
-
-        for(String address : addressList){
-            ret = ret + address;
-        }
-
-        return ret;
-    }*/
 }
