5e679de5e1d37282ab6752165384adec3752d136
Ubuntu_16_04_kms <his4000@hanmail.net>
2017-10-16 07:33:17 -0700

    Make simulation system

--- Simulator/Simulator.c
+++ Simulator/Simulator.c
@@ -29,17 +29,9 @@
 int generateRandomNumber(){
 	int ret;
 
-	srand(time(NULL));
 	ret = rand() % 15;
 
 	return ret;
-}
-
-void secDelay(int clock){
-	int i;
-
-	for(i=0;i<clock;i++)
-		delay(1000);
 }
 
 void sendData(float GPS_X, float GPS_Y)
@@ -51,6 +43,7 @@ void sendData(float GPS_X, float GPS_Y)
 	struct Result newResult;		/* Declare Struct */
 	int randomVal = generateRandomNumber();
 
+	srand(time(NULL));
 	/* Create a UDP socket */
 	sock = socket(PF_INET, SOCK_DGRAM, 0);
 
@@ -67,7 +60,7 @@ void sendData(float GPS_X, float GPS_Y)
 	server_addr.sin_addr.s_addr = inet_addr(clntIP);
 
 	newResult.dust_pm25 = MIN_PM_25 + randomVal;
-	newResult.dust_pm10 = MIN_PM_50 + (randomVal*2);
+	newResult.dust_pm10 = MIN_PM_10 + (randomVal*2);
 	newResult.GPS_X = GPS_X;
 	newResult.GPS_Y = GPS_Y;
 
@@ -87,6 +80,55 @@ void sendData(float GPS_X, float GPS_Y)
 
 }
 
+int isInRange(float gps_x, float gps_y){
+	int ret = 1;
+
+	if(gps_x < MIN_GPS_LAT || gps_x > MAX_GPS_LAT)
+		ret = 0;
+	if(gps_y < MIN_GPS_LNG || gps_y > MAX_GPS_LNG)
+		ret = 0;
+
+	return ret;
+}
+
+void moving(float* gps_x, float* gps_y){
+	int dir = rand()%4;
+	float current_gps_x = *gps_x;
+	float current_gps_y = *gps_y;
+
+	switch(dir){
+		case 0 : //Move East
+			current_gps_y -= MOVE_RANGE;
+			break;
+		case 1 : //Move West
+			current_gps_y += MOVE_RANGE;
+			break;
+		case 2 : //Move South
+			current_gps_x -= MOVE_RANGE;
+			break;
+		case 3 : 
+			current_gps_x += MOVE_RANGE;
+			break;
+		default : 
+			break;
+	}
+
+	if(isInRange(current_gps_x, current_gps_y)){
+		(*gps_x) = current_gps_x;
+		(*gps_y) = current_gps_y;
+	}
+}
+
 int main(void){
-	
+	float current_gps_x = MAX_GPS_LAT;
+	float current_gps_y = MAX_GPS_LNG;
+
+	while(1){
+		moving(&current_gps_x, &current_gps_y);
+		sendData(current_gps_x, current_gps_y);
+
+		sleep(DELAY_TIME);
+	}
+
+	return 0;
 }
