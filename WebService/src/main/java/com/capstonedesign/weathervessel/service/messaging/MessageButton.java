c4cc020785c050d5b9ccbc7d73c94d66a4bf6990
KimMinSu-PC\KimMinSu <his4000@hanmail.net>
2017-10-18 21:47:03 +0900

    Change service button

--- WebService/src/main/java/com/capstonedesign/weathervessel/service/messaging/MessageButton.java
+++ WebService/src/main/java/com/capstonedesign/weathervessel/service/messaging/MessageButton.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class MessageButton {
 
-    private final String label = "미세먼지 서비스 바로가기";
+    private final String label = "서비스 바로가기";
     private String url;
 
     public MessageButton(String url){
