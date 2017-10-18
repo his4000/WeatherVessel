7d2ee6a0119eef55640360049b6abebe42b2d7c1
KimMinSu-PC\KimMinSu <his4000@hanmail.net>
2017-10-19 07:28:20 +0900

    Change web page UI

--- WebService/src/main/java/com/capstonedesign/weathervessel/domain/ObserveRepository.java
+++ WebService/src/main/java/com/capstonedesign/weathervessel/domain/ObserveRepository.java
@@ -1,9 +1,11 @@
 package com.capstonedesign.weathervessel.domain;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ObserveRepository extends CrudRepository<Observe, Integer> {
     List<Observe> findObserveByAddrIdOrderByTimeDesc(Address address_id);
+    //@Query("select * from observe o1 where o1.id == (select o2.id from observe o2 where )")
 }
