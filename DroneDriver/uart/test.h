38137dd4ecabe469e1102a11c23858f0dfe2cd4f
Minsu, Kim <his4000@hanmail.net>
2017-11-24 12:55:33 +0900

    Update test.h
    Delete blank

--- DroneDriver/uart/test.h
+++ DroneDriver/uart/test.h
@@ -1,14 +1,14 @@
-      1 #ifndef H_TEST
-      2 #define H_TEST
-      3 struct dusti2c
-      4 {
-      5     int num;
-      6     unsigned long dust_pm25;
-      7     unsigned long dust_pm10;
-      8     float gps_x;
-      9     float gps_y;
-     10 };
-     11 int dustsensor_send(struct dusti2c *msg);
-     12 int recordSensorData(struct dusti2c *data);
-     13
-     14 #endif
+#ifndef H_TEST
+#define H_TEST
+struct dusti2c
+{
+    int num;
+    unsigned long dust_pm25;
+    unsigned long dust_pm10;
+    float gps_x;
+    float gps_y;
+};
+int dustsensor_send(struct dusti2c *msg);
+int recordSensorData(struct dusti2c *data);
+
+#endif
